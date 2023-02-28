ab94a8656198b3aca42b3ecf7c9e9ce663847d06
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2023-02-28 15:51:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function changeText() {
     var paragraphs = document.getElementsByTagName("p");
+    document.body.style.backgroundColor = "black";
     for (var i = 0; i < paragraphs.length; i++) {
       paragraphs[i].innerHTML = "<b><i>" + paragraphs[i].innerHTML + "</i></b>";
     }
